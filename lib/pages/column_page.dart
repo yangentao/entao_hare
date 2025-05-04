f9ef90121e5ef62b2bb6a0c39c2cb76ad32d54f8
Entao <entaoyang@163.com>
2025-05-05 06:43:16 +0800

    0.0.3

--- lib/pages/column_page.dart
+++ lib/pages/column_page.dart
@@ -21,3 +21,34 @@ abstract class ColumnPage extends HarePage {
     return pd == null ? widget : widget.padded(pd);
   }
 }
+
+abstract class ColumnPageX extends HarePage with RefreshMixin {
+  EdgeInsets? padding;
+
+  ColumnPageX() : super();
+
+  List<Widget> buildWidgets(BuildContext context);
+
+  List<Widget> aboveWidgets(BuildContext context) {
+    return [];
+  }
+
+  List<Widget> belowWidgets(BuildContext context) {
+    return [];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget body = ColumnMaxStretch(buildWidgets(context));
+    body = decorateBody(body);
+    var ls1 = aboveWidgets(context);
+    var ls2 = belowWidgets(context);
+    if (ls1.isEmpty && ls2.isEmpty) return body.verticalScroll();
+    return ColumnMaxStretch([...ls1, body.verticalScroll().expanded(), ...ls2]);
+  }
+
+  Widget decorateBody(Widget body) {
+    var pd = padding;
+    return pd == null ? body : body.padded(pd);
+  }
+}
